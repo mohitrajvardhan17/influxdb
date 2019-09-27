f5e5d7d1145a072656e88b72745d5f0a6dc43888
Deary Hudson <dhudson@influxdata.com>
2019-09-27 16:16:26 -0500

    updates tests

--- http/user_test.go
+++ http/user_test.go
@@ -2,9 +2,6 @@ package http
 
 import (
 	"context"
-	"fmt"
-	"io/ioutil"
-	http "net/http"
 	"net/http/httptest"
 	"testing"
 
@@ -12,7 +9,6 @@ import (
 	"github.com/influxdata/influxdb/inmem"
 	"github.com/influxdata/influxdb/mock"
 	platformtesting "github.com/influxdata/influxdb/testing"
-	"github.com/julienschmidt/httprouter"
 	"go.uber.org/zap"
 )
 
@@ -56,105 +52,4 @@ func initUserService(f platformtesting.UserFields, t *testing.T) (platform.UserS
 func TestUserService(t *testing.T) {
 	t.Parallel()
 	platformtesting.UserService(initUserService, t)
-}
-
-func TestService_handleGetUser(t *testing.T) {
-	type fields struct {
-		UserService platform.UserService
-	}
-	type args struct {
-		id string
-	}
-	type wants struct {
-		statusCode  int
-		contentType string
-		body        string
-	}
-
-	tests := []struct {
-		name   string
-		fields fields
-		args   args
-		wants  wants
-	}{
-		{
-			name: "get a user with no status defaults to `active`",
-			fields: fields{
-				&mock.UserService{
-					FindUserByIDFn: func(ctx context.Context, id platform.ID) (*platform.User, error) {
-						if id == platformtesting.MustIDBase16("020f755c3c082000") {
-							return &platform.User{
-								ID:   platformtesting.MustIDBase16("020f755c3c082000"),
-								Name: "myname",
-							}, nil
-						}
-
-						return nil, fmt.Errorf("not found")
-					},
-				},
-			},
-			args: args{
-				id: "020f755c3c082000",
-			},
-			wants: wants{
-				statusCode:  http.StatusOK,
-				contentType: "application/json; charset=utf-8",
-				body: `
-		{
-		  "links": {
-		    "self": "/api/v2/users/020f755c3c082000",
-		    "logs": "/api/v2/users/020f755c3c082000/logs"
-		  },
-		  "id": "020f755c3c082000",
-		  "name": "myname",
-		  "status": "active"
-		}
-		`,
-			},
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			userBackend := NewMockUserBackend()
-			userBackend.HTTPErrorHandler = ErrorHandler(0)
-			userBackend.UserService = tt.fields.UserService
-			h := NewUserHandler(userBackend)
-
-			r := httptest.NewRequest("GET", "http://any.url", nil)
-
-			r = r.WithContext(context.WithValue(
-				context.Background(),
-				httprouter.ParamsKey,
-				httprouter.Params{
-					{
-						Key:   "id",
-						Value: tt.args.id,
-					},
-				}))
-
-			w := httptest.NewRecorder()
-
-			h.handleGetUser(w, r)
-
-			res := w.Result()
-			content := res.Header.Get("Content-Type")
-			body, _ := ioutil.ReadAll(res.Body)
-			t.Logf(res.Header.Get("X-Influx-Error"))
-
-			if res.StatusCode != tt.wants.statusCode {
-				t.Errorf("%q. handleGetUser() = %v, want %v", tt.name, res.StatusCode, tt.wants.statusCode)
-			}
-			if tt.wants.contentType != "" && content != tt.wants.contentType {
-				t.Errorf("%q. handleGetUser() = %v, want %v", tt.name, content, tt.wants.contentType)
-			}
-			if tt.wants.body != "" {
-				if eq, diff, err := jsonEqual(string(body), tt.wants.body); err != nil {
-					t.Errorf("%q, handleGetUser(). error unmarshaling json %v", tt.name, err)
-				} else if !eq {
-					t.Errorf("%q. handleGetUser() = ***%s***", tt.name, diff)
-				}
-			}
-		})
-	}
 }
